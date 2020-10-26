52d67abca6b540c402873aed2a4a350526b38f62
Matti P <mpuupera@gmail.com>
2020-10-26 15:33:46 +0200

    changes

--- task1.js
+++ task1.js
@@ -5,19 +5,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// ask user for the anme input
 rl.question(`number 1 `, (number1) => {
 
-    // ask for nationality
     rl.question(`number 2 `, (number2) => {
 
-        // log user details
         console.log('sum of two numbers '+(parseInt(number1)+parseInt(number2)));
         console.log('difference of two numbers '+(parseInt(number1)-parseInt(number2)));
         console.log('fractions of two numbers '+(parseInt(number1)/parseInt(number2)));
         console.log('average of two numbers '+(parseInt(number1)+parseInt(number2))/2);
         
-        // close the stream
         rl.close();
     });
 
