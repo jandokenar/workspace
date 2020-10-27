e90d47cd19865c95e428866b512e6b0a702883e8
Matti P <mpuupera@gmail.com>
2020-10-27 08:46:16 +0200

    update

--- ifelse.js
+++ ifelse.js
@@ -1,12 +1,3 @@
-/*
-If/else
-
-Create a program that takes in two numbers a and b from the command line
-Print out "a is greater" if a is bigger than b, and vice versa, and they are equal if they are equal
-Modify program to take in a third string argument c, and print out "yay, you guessed the password", if a
-and b are equal AND c is "hello world"
-*/
-
 //first parameter
 const a = parseInt(process.argv[2]);
 
