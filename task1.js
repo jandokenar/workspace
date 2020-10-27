49e4bfa9e44c52ab0d409f855aca6bdeca2aabca
Matti P <mpuupera@gmail.com>
2020-10-27 08:28:00 +0200

    update

--- task1.js
+++ task1.js
@@ -1,10 +1,10 @@
 //first parameter
-const number1 = parseInt(process.argv[2]);
+const a = parseInt(process.argv[2]);
 
 //second parameter
-const number2 = parseInt(process.argv[3]);
+const b = parseInt(process.argv[3]);
 
- console.log('sum of two numbers '+(number1+number2));
- console.log('difference of two numbers '+(number1-number2));
- console.log('fractions of two numbers '+(number1/number2));
- console.log('average of two numbers '+(number1+number2)/2);
+ console.log('sum of two numbers '+(a+b));
+ console.log('difference of two numbers '+(a-b));
+ console.log('fractions of two numbers '+(a/b));
+ console.log('average of two numbers '+(a+b)/2);
