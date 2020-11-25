93a7675f33e053f29f768c79d2c06f49dad5d42d
Matti P <mpuupera@gmail.com>
2020-11-25 09:18:12 +0200

    more fixes

--- bank-frontend/bank/src/Inputlogin.jsx
+++ bank-frontend/bank/src/Inputlogin.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 const Inputlogin = (props) => {
-    const { getData } = props;
+    const { getUserData } = props;
     const [text, setText] = useState();
 
     return (
 
-        <div><input type="number" onChange={(e) => setText(e.target.value)} />
-            &nbsp;&nbsp;<button onClick={() => getData(text)}>Login</button>
+        <div><input type="number" placeholder="Enter your id" onChange={(e) => setText(e.target.value)} />
+            &nbsp;&nbsp;<button onClick={() => getUserData(text)}>Login</button>
         </div>
 
     );
