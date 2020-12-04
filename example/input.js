f4b82f365173f19d6b11672431ed74bc48ec2d6f
jandokenar <75470185+jandokenar@users.noreply.github.com>
2020-12-04 09:58:56 +0200

    Delete input.js

--- example/input.js
+++
@@ -1,6 +0,0 @@
-// requires "type" : "module" in package.json
-
-import readline from "readline-sync";
-
-const answer = readline.question("Tell me something :");
-console.log(`your answer ${answer}`);
